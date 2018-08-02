47260876f445b936e9167b6c4f7c759440f99f6d
acroos <roos.austin@gmail.com>
2018-08-03 00:18:13 +0200

    Fixed typo

---
+++ server.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+const staticFiles = express.static(path.join(__dirname, 'dist'))
+app.use(staticFiles)
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(3000, function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log('Listening at http://localhost:3000');
+});
